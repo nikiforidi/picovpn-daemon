ddab273999cfc0f00172f022b45cef1b47dba1cb
anatolio-deb <nikiforova693@gmail.com>
2025-06-26 12:27:11 +0000

    refactor host

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"github.com/anatolio-deb/picovpnd/api"
 	"github.com/anatolio-deb/picovpnd/core"
 	pb "github.com/anatolio-deb/picovpnd/grpc"
-	"github.com/anatolio-deb/picovpnd/ip"
 	"golang.org/x/crypto/acme/autocert"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -68,30 +67,15 @@ func (s *server) UserChangePassword(context.Context, *pb.UserChangePasswordReque
 
 // https://github.com/grpc/grpc-go/blob/master/examples/features/encryption/TLS/server/main.go
 func main() {
-	ip, err := ip.GetPublicIP()
-	if err != nil {
-		log.Fatalf("failed to get public IP: %v", err)
-	}
-	// err = auth.GenerateSelfSignedCert(certFile, keyFile, []string{ip})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	names, err := net.LookupAddr(ip)
-	if err != nil || len(names) == 0 {
-		log.Fatalf("failed to lookup domain name for IP %s: %v", ip, err)
-	} else {
-		log.Printf("domain name for IP %s: %s", ip, names[0])
-	}
-
+	host := os.Getenv("DAEMON_HOST")
 	m := &autocert.Manager{
 		Cache:      autocert.DirCache("certs"),
 		Prompt:     autocert.AcceptTOS,
 		Email:      os.Getenv("AUTOCERT_EMAIL"),
-		HostPolicy: autocert.HostWhitelist(names[0]),
+		HostPolicy: autocert.HostWhitelist(host),
 	}
 	cert, err := m.GetCertificate(&tls.ClientHelloInfo{
-		ServerName: names[0],
+		ServerName: host,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -121,7 +105,7 @@ func main() {
 	log.Printf("listening on %s", lis.Addr().String())
 
 	daemon := api.Daemon{
-		Address: names[0],
+		Address: host,
 		Port:    lis.Addr().(*net.TCPAddr).Port,
 		CertPEM: cert.Certificate[0],
 		// KeyPem:  key,
